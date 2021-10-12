722628128fefbe5123eea001403bccc331e807ec
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2021-10-12 19:54:27 +0300

    Update and rename CustomExample to CustomExample.java

---
+++ CustomExample.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.ArrayList;
+
+import collections.SimpleList;
+
+public class Application {
+	public static void main(String[] args) {
+
+		SimpleList sList = new SimpleList();
+		sList.add(10);
+		sList.add(20);
+		sList.add(30);
+
+		System.out.println("The list has " + sList.getLength() + " elements");
+
+		sList.print();
+	}
+}
+
+
