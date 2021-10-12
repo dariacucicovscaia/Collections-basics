ffe745fe1d66b56c5a171c3bede6f3007a6f0c82
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2021-10-12 19:54:59 +0300

    Update CustomExample.java

--- CustomExample.java
+++ CustomExample.java
@@ -18,4 +18,94 @@ public class Application {
 	}
 }
 
+package collections;
 
+import java.util.ArrayList;
+
+public class SimpleList {
+
+	// properties
+	private int length;
+	private Box first;
+	private int value;
+
+	// private data type
+	private class Box {
+		static int value;
+		Box next;
+
+		public static int getValue() {
+			return value;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		}
+
+		public Box getNext() {
+			return next;
+		}
+
+		public void setNext(Box next) {
+			this.next = next;
+		}
+
+		public Box(int value) {
+			super();
+			this.value = value;
+		}
+
+		public Box(Box next) {
+			super();
+			this.next = next;
+		}
+
+	
+
+	}
+
+	
+	public SimpleList() {
+		length = 0;
+		first = null;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public Box getFirst() {
+		return first;
+	}
+
+	public void setFirst(Box first) {
+		this.first = first;
+	}
+
+	public void add(int value) {
+		Box current = first;
+		if (length == 0) {
+			first = new Box(value);
+		} else {
+			for (int i = 0; i < length - 1; i++) {
+				current = current.next;
+			}
+			current.next = new Box(value);
+		}
+			length++;
+	  
+			}
+	
+	
+	public void print() {
+		for (int i = 0; i <length; i++) {
+			System.out.println(Box.getValue());
+		}
+		
+	}
+			
+	}
